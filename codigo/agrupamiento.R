286c4272fc4a45161a81496287e83879972fca1c
diagiraldo <dia.giraldo@gmail.com>
2021-02-17 16:50:33 -0500

    presentación version 0.9

--- codigo/agrupamiento.R
+++ codigo/agrupamiento.R
@@ -19,11 +19,12 @@ library(factoextra)
 library(NbClust)
 
 # Necesitamos unicamente con los puntajes cognitivos de las personas con MCI:
-B <- A %>%
-    filter(dx == "MCI") %>%
+Amci <- filter(A, dx == "MCI")
+B <- Amci %>%
     select(MEMORY, LANGUAGE, EXECUTIVE, 
            VISUOSPATIAL, ORIENTATION, ATTENTION)
 
+set.seed(123)
 # Primer ejemplo: k-means con k=2
 k <- 2
 k2res <- kmeans(B, k)
@@ -39,23 +40,8 @@ table(k2res$cluster, k3res$cluster)
 
 # Visualizar los resultados: 
 # Factoextra los grafica en as 2 direcciones de mayor varianza.
-p2 <- fviz_cluster(k2res, data = B, geom = "point", pointsize = 0.75)
-p3 <- fviz_cluster(k3res, data = B, geom = "point", pointsize = 0.75)
-
-ggsave("presentacion/imgs/fviz_k2res.png", p2, width = 14, height = 12, units = "cm", dpi = 300, bg = "transparent")
-ggsave("presentacion/imgs/fviz_k3res.png", p3, width = 14, height = 12, units = "cm", dpi = 300, bg = "transparent")
-
-pl <- fviz_cluster(kmeans(B, 4), data = B, geom = "point", pointsize = 0.75)
-ggsave("presentacion/imgs/fviz_k4res.png", pl, width = 14, height = 12, units = "cm", dpi = 300, bg = "transparent")
-
-pl <- fviz_cluster(kmeans(B, 5), data = B, geom = "point", pointsize = 0.75)
-ggsave("presentacion/imgs/fviz_k5res.png", pl, width = 14, height = 12, units = "cm", dpi = 300, bg = "transparent")
-
-pl <- fviz_cluster(kmeans(B, 6), data = B, geom = "point", pointsize = 0.75)
-ggsave("presentacion/imgs/fviz_k6res.png", pl, width = 14, height = 12, units = "cm", dpi = 300, bg = "transparent")
-
-pl <- fviz_cluster(kmeans(B, 7), data = B, geom = "point", pointsize = 0.75)
-ggsave("presentacion/imgs/fviz_k7res.png", pl, width = 14, height = 12, units = "cm", dpi = 300, bg = "transparent")
+fviz_cluster(k2res, data = B, geom = "point", pointsize = 0.75)
+fviz_cluster(k3res, data = B, geom = "point", pointsize = 0.75)
 
 # K-means y el resto de métodos tiene inicialización aleatoria 
 k <- 5
@@ -70,58 +56,54 @@ ggsave("presentacion/imgs/fviz_k5resA.png", pl, width = 14, height = 12, units =
 pl <- fviz_cluster(res2, data = B, geom = "point", pointsize = 0.75)
 ggsave("presentacion/imgs/fviz_k5resB.png", pl, width = 14, height = 12, units = "cm", dpi = 300, bg = "transparent")
 
-# ----------------------------------------
-# Varianza intra grupo - within cluster sum of squares (wcss)
+# Método del codo con within cluster sum of squares (wcss)
 k2res$withinss
+k3res$withinss
 
+set.seed(123)
 
-set.seed(1995)
-
-k <- 2
-res <- kmeans(B, k)
-res$withinss
-res$tot.withinss
-
-# suma de varianzas intra clase < varianza total 
-res$totss
-res$tot.withinss
-res$betweenss
-
-# A mayor número de grupos, menor es la suma de varianzas intra clase 
-set.seed(2020)
-df <- data.frame(k = 1:10, tot.wcss = numeric(10))
+dfwcss <- data.frame(k = 1:10, tot.wcss = numeric(10))
 for (k in 1:10){
-    df$tot.wcss[k]  <- kmeans(B, k)$tot.withinss
+    kres <-  kmeans(B, k)
+    dfwcss$tot.wcss[k]  <- kres$tot.withinss
+    print(kres$withinss)
+    pl <- fviz_cluster(kres, data = B, geom = "point", pointsize = 0.75)
+    out_plotname <- sprintf("presentacion/imgs/fviz_k%dres.png", k)
+    ggsave(out_plotname, pl, width = 14, height = 12, units = "cm", dpi = 300, bg = "transparent")
 }
 
-ggplot(df, aes(k, tot.wcss)) + geom_point() + geom_line()
+pl <- ggplot(dfwcss, aes(k, tot.wcss)) + geom_point() + geom_line() + scale_x_continuous(breaks = 1:10)
+ggsave("presentacion/imgs/wcss.png", pl, width = 16, height = 8, units = "cm", dpi = 300, bg = "transparent")
 
 # en un solo comando 
-fviz_nbclust(B, kmeans, method = "wss")
+pl <- fviz_nbclust(B, kmeans, method = "wss")
+ggsave("presentacion/imgs/fviz_wss.png", pl, width = 16, height = 8, units = "cm", dpi = 300, bg = "transparent")
 
-# elbow method ---> nothing clear 
+# Método de la silueta
+pl <- fviz_nbclust(B, kmeans, method = "silhouette")
+ggsave("presentacion/imgs/fviz_silueta.png", pl, width = 16, height = 8, units = "cm", dpi = 300, bg = "transparent")
 
-# Metodo de la silueta 
-fviz_nbclust(B, kmeans, method = "silhouette")
+# Método del gap statistic
+pl <- fviz_nbclust(B, kmeans, method = "gap_stat")
+ggsave("presentacion/imgs/fviz_gap.png", pl, width = 16, height = 8, units = "cm", dpi = 300, bg = "transparent")
 
-# Gap statistic
-fviz_nbclust(B, kmeans, method = "gap_stat")
-
-# Y otro tanto de índices
+# 30 índices
 NK <- NbClust(data = B, method = "kmeans")
-
 # El número "recomendado" de grupos es 3
-k <- 3
-res <- kmeans(B, k)
-A$grupo3 <- as.factor(res$cluster)
 
+# Agrupamiento con k = 3
+res <- kmeans(B, 3)
 table(res$cluster)
-table(A$dx, res$cluster)
 
+# Descripción de los subgrupos
+Amci$k3 <- as.factor(res$cluster)
 nd <- colnames(B)
-m <- melt(A, measure.vars = nd)
-head(m)
-ggplot(m, aes(grupo3, value, colour = variable)) + geom_boxplot()
+m <- melt(Amci, measure.vars = nd)
+pl <- ggplot(m, aes(k3, value, colour = variable)) + geom_boxplot() + theme(legend.position = "top")
+ggsave("presentacion/imgs/boxplots3.png", pl, width = 12, height = 12, units = "cm", dpi = 300, bg = "transparent")
+
+
+
 
 
 
